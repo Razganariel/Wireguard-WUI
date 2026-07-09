70711ce6a9ebc276d56af381fbb630572c86025d
Razganariel <dvergar@hotmail.fr>
2026-07-09 22:34:54 +0200

    add csrf

---
+++ middlewares/csrf.js
@@ -0,0 +1,23 @@
+const Tokens = require('csrf')
+
+const tokens = new Tokens()
+
+function csrfMiddleware(req, res, next) {
+  if (!req.session.csrfSecret) {
+    req.session.csrfSecret = tokens.secretSync()
+  }
+
+  res.locals.csrfToken = tokens.create(req.session.csrfSecret)
+
+  if (req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE') {
+    const csrfBody = req.body._csrf
+    if (!csrfBody || !tokens.verify(req.session.csrfSecret, csrfBody)) {
+      req.session.flash = { error: 'Session invalide. Veuillez réessayer.' }
+      return res.redirect(req.get('Referer') || '/')
+    }
+  }
+
+  next()
+}
+
+module.exports = csrfMiddleware
